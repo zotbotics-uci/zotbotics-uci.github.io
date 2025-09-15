6ec3c1f1a0ca90c3441c53a50dcdf14d3840a7cf
Varshini Vijay <varshinivijay05@gmail.com>
2025-09-14 22:50:35 -0700

    trying to make the correct url work

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
-,   // root deployment
+  base: '/zims/',   // must match the repo name
 })
