4b5074c0e296e79308be8fe6ffe16c7cd30ab289
Varshini VIjay <99629838+varshinivij@users.noreply.github.com>
2025-09-16 20:21:51 +0000

    functional ver1.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <div className="nav-inner">
-        <Link to="/" className="brand">
+        <Link to="/zims/" className="brand">
           <img src={asset('image1.png')} alt="logo" />
           <span>ZIMS</span>
         </Link>
         <div className="nav-links">
-          <NavLink to="/about" className={({isActive})=>`nav-link ${isActive?'active':''}`}>About</NavLink>
-          <NavLink to="/makerspace" className={({isActive})=>`nav-link ${isActive?'active':''}`}>MAKERSPACE</NavLink>
-          <NavLink to="/projects" className={({isActive})=>`nav-link ${isActive?'active':''}`}>PROJECTS</NavLink>
+          <NavLink to="/zims/about" className={({isActive})=>`nav-link ${isActive?'active':''}`}>About</NavLink>
+          <NavLink to="/zims/makerspace" className={({isActive})=>`nav-link ${isActive?'active':''}`}>MAKERSPACE</NavLink>
+          <NavLink to="/zims/projects" className={({isActive})=>`nav-link ${isActive?'active':''}`}>PROJECTS</NavLink>
         </div>
       </div>
     </nav>
